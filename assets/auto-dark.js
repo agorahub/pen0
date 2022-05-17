d4772f6615c6e2230510562b3f6a92cdefb0d2d7
Plaskier <plaskier@protonmail.com>
2022-05-17 10:22:49 +0800

    Dark mode optimization. - Auto dark mode based on system preference. - Manual toggle switch at the bottom of the page. - Instant and dynamic responding.

---
+++ assets/auto-dark.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
